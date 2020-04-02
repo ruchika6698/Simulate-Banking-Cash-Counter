8d450d986af8939229d858b142725222b62faeb4
ruchika <ruchika6698@gmail.com>
2020-04-02 14:38:00 +0530

    Added:Welcome to Bank Cash Counter

---
+++ BankingCash.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banking_Cash_Counter
+{
+    class BankingCash
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Banking Cash Counter");
+        }
+    }
+}
