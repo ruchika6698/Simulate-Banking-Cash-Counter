3c6474c5194224b45ae9c7077dd4b1cf4afcf7d3
ruchika <ruchika6698@gmail.com>
2020-04-03 11:40:41 +0530

    Added:Banking cash counter using Stack

--- BankingCash.cs
+++ BankingCash.cs
@@ -6,14 +6,67 @@ namespace Banking_Cash_Counter
 {
     class BankingCash
     {
+        //Stack
+        class Stack
+        {
+            public int Count { get; private set; }
+            private int[] stack = new int[6];
+            public Stack(int size)
+            {
+                stack = new int[size];
+                Count = 0;
+            }
+            public Stack()
+            {
+            }
+            //push customer 
+            public void Push(int s)
+            {
+                if (Count != stack.Length) 
+                {
+                    stack[Count++] = s;
+                }
+                else
+                {
+                    Console.WriteLine("Stack is full!");
+                }
+            }
+            //pop Customer after operations Completed
+            public int Pop()
+            {
+                int s = -1;
+                if (Count == 0)
+                {
+                    Console.WriteLine("Stack is empty!");
+                }
+                else
+                {
+                    s = stack[--Count];
+                }
+                return s;
+            }
+            public void Display()
+            {
+                if (Count == 0)
+                {
+                    Console.WriteLine("Stack is empty!");
+                }
+                else
+                {
+                    Console.WriteLine("Stack data:");
+                    for (int i = Count - 1; i >= 0; i--)
+                        Console.WriteLine(stack[i]);
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            //constant
+        //constant
             int BALANCE = 5000;
             int M = 3;
-
             //Enqueue and Dequeue
             Queue customers = new Queue();
+            Stack s = new Stack();
 
             customers.Enqueue(1);
             customers.Enqueue(2);
@@ -22,6 +75,14 @@ namespace Banking_Cash_Counter
             customers.Enqueue(5);
             customers.Enqueue(6);
             Console.WriteLine("Number of Customers in the Queue: {0}", customers.Count);
+
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            s.Push(4);
+            s.Push(5);
+            s.Push(6);
+            Console.WriteLine("Number of Customers in the Stack:{0}", s.Count);
 
             Console.WriteLine("Welcome to Banking Cash Counter");
             Console.WriteLine("1.Deposit cash");
@@ -56,6 +117,7 @@ namespace Banking_Cash_Counter
                 }
                 Console.WriteLine(customers.Dequeue());
                 Console.WriteLine("Number of Customers in the Queue: {0}", customers.Count);
+                Console.WriteLine(s.Pop() + " Popped from stack");
             }
         }
     }
